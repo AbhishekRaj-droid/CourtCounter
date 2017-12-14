5801a7785e01015c374f4e1535e0425c42393f77
deepclearning <deepclearning@gmail.com>
2017-12-15 01:06:18 +0530

    Introduced Team A and Team B with functionality

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreA = 0;
+    int scoreB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamB(scoreB);
+        displayForTeamA(scoreA);
+    }
+
+    private void displayForTeamA (int score) {
+
+        TextView teamA_score = (TextView) findViewById(R.id.scoreteamA);
+        teamA_score.setText(String.valueOf(score));
+    }
+
+    private void displayForTeamB (int score) {
+
+        TextView teamB_score = (TextView) findViewById(R.id.scoreteamB);
+        teamB_score.setText(String.valueOf(score));
+    }
+
+    public void submit3point (View view) {
+        scoreA += 3;
+        displayForTeamA(scoreA);
+    }
+
+    public void submit2point (View view) {
+        scoreA += 2;
+        displayForTeamA(scoreA);
+    }
+
+    public void submit1point (View view) {
+        scoreA += 1;
+        displayForTeamA(scoreA);
+    }
+
+    public void submit3pointb (View view) {
+        scoreB += 3;
+        displayForTeamB(scoreB);
+    }
+
+    public void submit2pointb (View view) {
+        scoreB += 2;
+        displayForTeamB(scoreB);
+    }
+
+    public void submit1pointb (View view) {
+        scoreB += 1;
+        displayForTeamB(scoreB);
     }
 }
